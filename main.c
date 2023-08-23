8ed6f91adef8088c1a65b5c50515fbefecdf77a6
amos zohar <azamoszohar@gmail.com>
2023-08-23 16:02:00 +0300

    finished ex

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
+/*name: Amos Zohar, ID: 311402812*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 void generate_random_array(int* arr, int n) {
-    //extra memory - accessory array
+    /*I have used an accessory array,it is manually allocated since n is random.*/
     int* ascending_numbers = (int*)malloc(n * sizeof(int));
-    /*first, I generate random values*/
+    /*first, I generate random values, between 1 and 10000, including both.*/
     for (int i = 0; i < n; i++) {
         ascending_numbers[i] = rand() % 10000 + 1;
     }
@@ -20,41 +21,44 @@ void generate_random_array(int* arr, int n) {
             }
         }
     }
-    /*Now that I have a sorted size n accessory array I copy past the values to arr*/
+    /*Now that I have a sorted size n accessory array I copy and past the values to arr*/
     for (int i = 0; i < n; i++) {
         arr[i] = ascending_numbers[i];
     }
-    /*Now that the values has been copied, I free the accessory array*/
+    /*Now that the values has been copied to the original array, I free the accessory array*/
     free(ascending_numbers); 
 }
-int arrayIsValid(int arr[],int M, int n){
-    int numOfZeros = 0;
-    int positiveOrdered = 1;//assume true
-    for (int i = 0; i < n-1; i++) {
-        if (arr[i] > arr[i + 1]) {
-            positiveOrdered = 0;
-            break;
-        }
-    }
-    int zerosFlag = 1;
-    for (int i = n; i < M; i++) {
-        if (arr[i] != 0) {
-            zerosFlag = 0;
-            break;
-        }
-        numOfZeros++;
-    }
-    return positiveOrdered * zerosFlag *(1-(n-numOfZeros));
-}
-void printArray(int arr[], int M) {
-    for (int i = 0; i < M; i++) {
-        if (i % 100 == 0) {
-            printf("\n\n");
-        }
-        printf("%d ", arr[i]);
-    }
-}
-int f1(int arr[],int M,int x,int* n1) {
+/* The 2 functions bellow were used by me to make sure that the array generated
+is ok. If you wish, you can uncomment and use them.*/
+
+//int arrayIsValid(int arr[],int M, int n){
+//    int positiveOrdered = 1;//assume true
+//    for (int i = 0; i < n-1; i++) {
+//        if (arr[i] > arr[i + 1]) {
+//            positiveOrdered = 0;
+//            break;
+//        }
+//    }
+//    int zerosFlag = 1;
+//    for (int i = n; i < M; i++) {
+//        if (arr[i] != 0) {
+//            zerosFlag = 0;
+//            break;
+//        }
+//    }
+//    return positiveOrdered * zerosFlag;
+//}
+//void printArray(int arr[], int M) {
+//    printf("\n/////////////////////////////////////////////////////////////////////////////////////////////////////");
+//    for (int i = 0; i < M; i++) {
+//        if (i % 100 == 0) {
+//            printf("\n");
+//        }
+//        printf("%d ", arr[i]);
+//    }
+//    printf("\n/////////////////////////////////////////////////////////////////////////////////////////////////////");
+//}
+int Search1(int arr[],int M,int x,int* n1) {
     (*n1) = 0;
     int val;
     for (int i = 0; i < M; i++) {
@@ -74,7 +78,7 @@ int f1(int arr[],int M,int x,int* n1) {
         }
     }
 }
-int f2(int arr[], int M, int x, int* n2) {
+int Search2(int arr[], int M, int x, int* n2) {
     int start_index = 0;//1 in psuedo-code
     int end_index = M - 1;//M in psuedo-code
     int mid_index;
@@ -114,7 +118,7 @@ int f2(int arr[], int M, int x, int* n2) {
     return -1;
 }
 
-int f3(int arr[], int M, int x, int* n3) {
+int Search3(int arr[], int M, int x, int* n3) {
     /*I will still perform a binary search, however, this time it will be done
     not for size M array, but for size R = 2^(1+Math.floor(log2(n))),
     which is at worst case 2n. Thus, by performing binary search on such input,
@@ -129,7 +133,7 @@ int f3(int arr[], int M, int x, int* n3) {
         binary-searching away*/
         (*n3)++;
         if (arr[R - 1] > x){
-            return f2(arr, R-1, x, &n3);//using the modified binary search f2
+            return Search2(arr, R-1, x, &n3);//using the modified binary search f2
         }
         (*n3)++;
         if (arr[R-1] == x) {//if by pure dumb luck, n sits in an index which is a power of 2
@@ -144,7 +148,7 @@ int f3(int arr[], int M, int x, int* n3) {
     }
     /*Now that I have R, a number that stays true to the condition n<=R<=2n(in our little world, worst case scenario: R=256),
     i can call binary search, which for R mean time-complexity of O(log2(R)), which nets us O(logn)*/
-    return f2(arr, R - 1, x, &n3);
+    return Search2(arr, R - 1, x, &n3);
 }
 
 int checkIfOneNeighbourIsTheSame(int arr[], int i,int n) {
@@ -170,57 +174,61 @@ int main() {
     int totalCmp1 = 0;
     int totalCmp2 = 0;
     int totalCmp3 = 0;
+    //int n = rand() % 101 + 150;
+    int avg_n = 0;
     for (int j = 0; j < num_arrays; j++) {
         int n = rand() % 101 + 150;
+        avg_n += n;
         generate_random_array(input_array, n);
         for (int i = n; i < 1000; i++) {
             input_array[i] = 0;
         }
-        if (!arrayIsValid(input_array,1000,n)) {
-            printf("\nARRAY NOT VALID\n");
-            return -1;
-        }
+        //if (!arrayIsValid(input_array,1000,n)) {
+        //    printf("\nARRAY NOT VALID\n");
+        //    return -1;
+        //}
+        //printArray(input_array, 1000);
         
         int randomNum = rand() % 10000 + 1;
         int numOfComparisons1 = 0;
         int numOfComparisons2 =0;
         int numOfComparisons3 =0;
-        int res1 = f1(input_array, 1000, randomNum,&numOfComparisons1);
-        int res2 = f2(input_array, 1000, randomNum,&numOfComparisons2);
+        int res1 = Search1(input_array, 1000, randomNum,&numOfComparisons1);
+        int res2 = Search2(input_array, 1000, randomNum,&numOfComparisons2);
         totalCmp1 += numOfComparisons1;
         totalCmp2 += numOfComparisons2;
-        printf("\nnumOfComparisons1 = %d,totalCmp1 = %d, n= %d", numOfComparisons1, totalCmp1,n);
-        printf("\nnumOfComparisons2 = %d,totalCmp2 = %d, n= %d\n", numOfComparisons2, totalCmp2,n);
-        int res3 = f3(input_array, 1000, randomNum, &numOfComparisons3);
+        //printf("\nnumOfComparisons1 = %d,totalCmp1 = %d, n= %d", numOfComparisons1, totalCmp1,n);
+        //printf("\nnumOfComparisons2 = %d,totalCmp2 = %d, n= %d\n", numOfComparisons2, totalCmp2,n);
+        int res3 = Search3(input_array, 1000, randomNum, &numOfComparisons3);
         totalCmp3 += numOfComparisons3;
-        printf("\nnumOfComparisons3 = %d,totalCmp3 = %d, n= %d\n", numOfComparisons3, totalCmp3, n);
-        if (res1 != res2 || res1!=res3) {
-            printf("res1 is %d, while res2 is %d, res3 is %d", res1, res2,res3);
-            if (res1 >= 0) {
-                if (checkIfOneNeighbourIsTheSame(input_array, res1, n)) {
-                    printf("\n res1 neighbour is the same");
-                }
-                else {
-                    printf("\n res1no duplicates. what does thie mean?");
-                }
-            }
-            if (res2 >= 0) {
-                if (checkIfOneNeighbourIsTheSame(input_array, res2, n)) {
-                    printf("\n res2 neighbour is the same");
-                }
-                else {
-                    printf("\n res2 no duplicates. what does thie mean?");
-                }
-            }
-            if (res3 >= 0) {
-                if (checkIfOneNeighbourIsTheSame(input_array, res3, n)) {
-                    printf("\n res3 neighbour is the same");
-                }
-                else {
-                    printf("\n res3 no duplicates. what does thie mean?");
-                }
-            }
-        }
+        //printf("\nnumOfComparisons3 = %d,totalCmp3 = %d, n= %d\n", numOfComparisons3, totalCmp3, n);
+        //if (res1 != res2 || res1!=res3) {
+        //    printf("res1 is %d, while res2 is %d, res3 is %d", res1, res2,res3);
+        //    if (res1 >= 0) {
+        //        if (checkIfOneNeighbourIsTheSame(input_array, res1, n)) {
+        //            printf("\n res1 neighbour is the same");
+        //        }
+        //        else {
+        //            printf("\n res1no duplicates. what does thie mean?");
+        //        }
+        //    }
+        //    if (res2 >= 0) {
+        //        if (checkIfOneNeighbourIsTheSame(input_array, res2, n)) {
+        //            printf("\n res2 neighbour is the same");
+        //        }
+        //        else {
+        //            printf("\n res2 no duplicates. what does thie mean?");
+        //        }
+        //    }
+        //    if (res3 >= 0) {
+        //        if (checkIfOneNeighbourIsTheSame(input_array, res3, n)) {
+        //            printf("\n res3 neighbour is the same");
+        //        }
+        //        else {
+        //            printf("\n res3 no duplicates. what does thie mean?");
+        //        }
+        //    }
+        //}
         //int randomCell = rand() % n;
 
         ////MBF stands for Must Be Found, since I asked to search for a value that must be in the array
@@ -239,8 +247,15 @@ int main() {
         //    }
         //}
     }
+    //NOTE TO TESTER:
+    //THE REQUEST TO PRINT 150<=n<=250 is a bit confusing.
+    //did you mean we print that immutable string as it is, or did you mean for us to print the average n?
+    //Or, should n have only been rolled once? but then it would be a bit less interesting.
+    //Anyway, dear tester, I hope you understand my confusion. I tried my best to understand
+    //that somewhat ambigious requirement.
+    avg_n /= 200;
     //printf("validCOunt = %d", validCount);
-    printf("\n\n150<=n<=250");
+    printf("\n\n150<=(Average n=%d)<=250",avg_n);
     float avg1 = totalCmp1 / 200;
     printf("\n\nSearch1:%f", avg1);
     float avg2 = totalCmp2 / 200;
